f5f2666cd092d6324fc87e98f217dba9caea400b
hacks.guide <128547338+Nomaakip@users.noreply.github.com>
2025-02-02 09:43:45 +0100

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,29 +1,3 @@
-function test() {
-
-    var elem = document.querySelectorAll(".post");
-    var i = 0, length = elem.length;
-    for ( ; i < length; i++) {
-        elem[i].style.display = "none";
-    }
-
-    document.getElementsByClassName("pagination")[0].innerHTML = `
-    <h1>PikiDiary+ Beta</h1>
-    <input id="idea" type="checkbox">post ideas</input>
-    <p>made with love by nomaakip, wish, and squirrel <3</p>
-    <a href="https://github.com/Stupid-Idiots-United/pikidiary-plus-beta/issues/new?q=sort%3Aupdated-desc+is%3Aissue+is%3Aopen&template=Blank+issue"><button>secret</button></a>`;
-    
-    if (ideac == "b") {
-        swag.style.display = "block";
-            document.getElementById('idea').checked = true;
-        }
-        if (ideac == "a") {
-        swag.style.display = "none";
-            document.getElementById('idea').checked = false;
-        }
-
-    document.getElementById('idea').addEventListener("click", ideae);
-
-}
 
 // array for post ideas
 const greg = new Array();
@@ -45,14 +19,6 @@ var swag = document.createElement('div');
 swag.innerHTML = "Dont know what to post? Here's a Post idea: " + greg[rand];
 document.getElementsByClassName("char-counter")[0].appendChild(swag);
 
-// adds the tab
-var newtab = document.createElement('a');
-newtab.innerHTML = 'PikiDiary+ ❤️';
-newtab.classList.add("tab");
-newtab.style.cursor = "pointer";
-document.getElementsByClassName("tab-cont")[0].appendChild(newtab);
-
-newtab.addEventListener("click", test);
 
 let ideac = localStorage.getItem("ideac");
 
@@ -84,3 +50,60 @@ swag.style.display = "none";
   localStorage.setItem("ideac", ideac);
 }
 }
+
+
+// adds the tab
+var newtab = document.createElement('a');
+newtab.innerHTML = 'PikiDiary+ ❤️';
+newtab.classList.add("tab");
+newtab.style.cursor = "pointer";
+document.getElementsByClassName("tab-cont")[0].appendChild(newtab);
+
+newtab.addEventListener("click", test);
+
+function test() {
+
+    var elem = document.querySelectorAll(".post");
+    var i = 0, length = elem.length;
+    for ( ; i < length; i++) {
+        elem[i].style.display = "none";
+    }
+
+    document.getElementsByClassName("pagination")[0].innerHTML = `
+    <h1 id="plus">PikiDiary+ Beta</h1>
+    <input id="idea" type="checkbox">post ideas</input>
+    <p id="luv">made with love by nomaakip, wish, and squirrel <3</p>
+    <a href="https://github.com/Stupid-Idiots-United/pikidiary-plus-beta/issues/new?q=sort%3Aupdated-desc+is%3Aissue+is%3Aopen&template=Blank+issue"><button id="shh">secret</button></a>`;
+
+    
+    const anim = [
+        {opacity: "0"},
+        {opacity: "1"}
+    ]
+
+    const animtiming = {
+        duration: 200,
+        iterations: 1
+      };
+
+      document.querySelectorAll(".tab.active")[0].classList.remove('active');
+
+      newtab.classList.add('active');
+
+      document.getElementsByClassName('pagination')[0].animate(anim, animtiming);
+
+
+    if (ideac == "b") {
+        swag.style.display = "block";
+            document.getElementById('idea').checked = true;
+        }
+        if (ideac == "a") {
+        swag.style.display = "none";
+            document.getElementById('idea').checked = false;
+        }
+
+    document.getElementById('idea').addEventListener("click", ideae);
+
+}
+
+
