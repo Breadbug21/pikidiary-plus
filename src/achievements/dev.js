24855e46e90129f6fa802e1729a633e4da67f546
hacks.guide <128547338+Nomaakip@users.noreply.github.com>
2025-02-07 14:32:54 +0100

    Add files via upload

--- src/achievements/dev.js
+++ src/achievements/dev.js
@@ -1,7 +1,7 @@
 document.getElementById('extra-achievements').style.display = 'block';
     var imgb = document.createElement("div");
 
-imgb.innerHTML = '<div style="display: flex; gap: 8px;"><img src="/img/appicon/icon-128x128.png" style="width: 44px; height: 44px; box-sizing: border-box; border: solid 1px #CCC; padding: 2px;"><div style="display: flex; flex-direction: column;"><b style="font-size: 19px;">[Piki+] #swag</b><span style="font-size: 11px;">PikiDiary+ developer</span></div></div><br>';
+imgb.innerHTML = '<div style=" margin-top:10px; display: flex; gap: 8px;"><img src="/img/appicon/icon-128x128.png" style="width: 44px; height: 44px; box-sizing: border-box; border: solid 1px #CCC; padding: 2px;"><div style="display: flex; flex-direction: column;"><b style="font-size: 19px;">[Piki+] #swag</b><span style="font-size: 11px;">PikiDiary+ developer</span></div></div><br>';
 
 document.getElementById('extra-achievements').appendChild(imgb);
 
