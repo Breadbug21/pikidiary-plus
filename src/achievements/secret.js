1802b829b0553342c7d1bb3127a574452076d971
mpax235 <millerpgraves@gmail.com>
2025-02-07 13:11:42 -0800

    fix badge not appearing on non-developers

--- src/achievements/secret.js
+++ src/achievements/secret.js
@@ -1,3 +1,4 @@
+document.getElementById('extra-achievements').style.display = 'block';
 var imgc = document.createElement("div");
 
 imgc.innerHTML = '<div style="display: flex; gap: 8px;"><img src="https://f.feridinha.com/CBjTc.jpg" style="width: 44px; height: 44px; box-sizing: border-box; border: solid 1px #CCC; padding: 2px;"><div style="display: flex; flex-direction: column;"><b style="font-size: 19px;">[Piki+] Lock & Key</b><span style="font-size: 11px;">This user found the secret in PikiDiary+!</span></div></div>';
